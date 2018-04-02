ad1f9bc53f68584f3e3cc458541870f0e12ba99d
RyotaNakaza <micrasoft.bf4@gmail.com>
2018-04-02 14:57:32 +0900

    ファイルコピー機能を実装した。

--- FileManager4Broadcasting/DupFilesForm.Designer.cs
+++ FileManager4Broadcasting/DupFilesForm.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.allProgres = new System.Windows.Forms.Label();
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -38,28 +38,27 @@
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(12, 24);
-            this.progressBar1.Maximum = 1000;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(440, 23);
             this.progressBar1.TabIndex = 0;
             // 
-            // label1
+            // fileNameLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(12, 9);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(50, 12);
+            this.fileNameLabel.TabIndex = 1;
+            this.fileNameLabel.Text = "fileName";
             // 
-            // label2
+            // allProgres
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.allProgres.AutoSize = true;
+            this.allProgres.Location = new System.Drawing.Point(12, 50);
+            this.allProgres.Name = "allProgres";
+            this.allProgres.Size = new System.Drawing.Size(75, 12);
+            this.allProgres.TabIndex = 2;
+            this.allProgres.Text = "全体の進捗度";
             // 
             // progressBar2
             // 
@@ -70,11 +69,11 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(377, 94);
+            this.cancelButton.Location = new System.Drawing.Point(364, 94);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.Size = new System.Drawing.Size(88, 23);
             this.cancelButton.TabIndex = 4;
-            this.cancelButton.Text = "キャンセル";
+            this.cancelButton.Text = "キャンセル(&C)";
             this.cancelButton.UseVisualStyleBackColor = true;
             this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
@@ -85,8 +84,8 @@
             this.ClientSize = new System.Drawing.Size(464, 129);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.progressBar2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.allProgres);
+            this.Controls.Add(this.fileNameLabel);
             this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -103,8 +102,8 @@
         #endregion
 
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label allProgres;
         private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.Button cancelButton;
     }
--- FileManager4Broadcasting/DupFilesForm.cs
+++ FileManager4Broadcasting/DupFilesForm.cs
@@ -15,6 +15,8 @@ namespace FileManager4Broadcasting
     {
         public Home home = new Home();
         private bool _stop = false;
+        private int count = 0;
+        private int filePathLength = 0;
 
         public DupFilesForm()
         {
@@ -24,30 +26,52 @@ namespace FileManager4Broadcasting
         private void DupFilesForm_Load(object sender, EventArgs e)
         {
             foreach (string s in home.filePaths)
+            {
                 MessageBox.Show(s);
+            }
         }
 
         private void DupFilesForm_Shown(object sender, EventArgs e)
         {
-            DupFile(home.filePaths[0], "");
+            progressBar2.Value = 0;
+            progressBar2.Maximum = home.filePaths.Length * 100;
+            filePathLength = home.filePaths.Length;
+            foreach (string s in home.filePaths)
+            {
+                count += 1;
+                DupFile(s, @"C:\Users\Owner\Desktop\テスト用フォルダ\");
+            }
+            Close();
         }
 
         void DupFile(string filePath,string directoryPath)
         {
-            PInvoke.Win32API copyObject = new PInvoke.Win32API();
-            copyObject.ProgressChanged += new PInvoke.Win32API.CopyProgressEventHandler(ProgressChanged);
             string fileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
             string extension = System.IO.Path.GetExtension(filePath);
-            PInvoke.Win32API.ResultStatus ret = copyObject.CopyStart(filePath, @"C:\Users\Owner\Desktop\テスト用フォルダ\"+fileName+"_duped"+extension, true);
+            fileNameLabel.Text = fileName + extension;
+            progressBar1.Value = 0;
+            PInvoke.Win32API copyObject = new PInvoke.Win32API();
+            copyObject.ProgressChanged += new PInvoke.Win32API.CopyProgressEventHandler(ProgressChanged);
+            PInvoke.Win32API.ResultStatus ret = copyObject.CopyStart(filePath,directoryPath+@"\"+fileName+extension, true);
+            switch (ret)
+            {
+                case PInvoke.Win32API.ResultStatus.Completed:
+                    break;
+                case PInvoke.Win32API.ResultStatus.Failed:
+                    MessageBox.Show("ファイルのコピーに失敗しました。");
+                    break;
+            }
         }
 
         void ProgressChanged(object s, PInvoke.Win32API.CopyProgressEventArgs e)
         {
             DoEvents();
             var progresspar = e.TotalFileSize > 0
-                ? ((decimal)e.TotalBytesTransferred / (decimal)e.TotalFileSize) * (decimal)1000
+                ? ((decimal)e.TotalBytesTransferred / (decimal)e.TotalFileSize) * (decimal)100
              : (decimal)0;
             progressBar1.Value = (int)progresspar;
+            progressBar2.Value = (int)progresspar + (count-1)*100;
+            Text = string.Format("{0}/{1}コピー中-{2}", count, filePathLength, fileNameLabel.Text); 
             /*Invoke(new Action(() =>
             {
                 
