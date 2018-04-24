f86cbaf0067322de73069c4734b2496965c45c81
RyotaNakaza <micrasoft.bf4@gmail.com>
2018-04-24 18:43:30 +0900

    modify import form

--- FileManager4Broadcasting/ImportForm.Designer.cs
+++ FileManager4Broadcasting/ImportForm.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -41,34 +39,15 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // treeView1
-            // 
-            this.treeView1.CheckBoxes = true;
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(121, 97);
-            this.treeView1.TabIndex = 2;
-            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(140, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // ImportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "ImportForm";
             this.Text = "ImportForm";
             this.Load += new System.EventHandler(this.ImportForm_Load);
@@ -81,7 +60,5 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Button button1;
     }
 }
--- FileManager4Broadcasting/ImportForm.cs
+++ FileManager4Broadcasting/ImportForm.cs
@@ -20,30 +20,6 @@ namespace FileManager4Broadcasting
         private void ImportForm_Load(object sender, EventArgs e)
         {
             
-            rootNode.Text = "root";
-            TreeNode testNode1 = new TreeNode();
-            testNode1.Text = "テストノード1";
-            TreeNode testNode2 = new TreeNode();
-            testNode2.Text = "テストノード2";
-            TreeNode childTestNode1 = new TreeNode();
-            childTestNode1.Text = "子テストノード1";
-            TreeNode childTestNode2 = new TreeNode();
-            childTestNode2.Text = "子テストノード2";
-            TreeNode childTestNode3 = new TreeNode();
-            childTestNode3.Text = "子テストノード3";
-            TreeNode childTestNode4 = new TreeNode();
-            childTestNode4.Text = "子テストノード4";
-
-            treeView1.Nodes.Add(rootNode);
-            rootNode.Nodes.Add(testNode1);
-            rootNode.Nodes.Add(testNode2);
-            testNode1.Nodes.Add(childTestNode1);
-            testNode1.Nodes.Add(childTestNode2);
-
-            testNode2.Nodes.Add(childTestNode3);
-            childTestNode3.Nodes.Add(childTestNode4);
-
-            
         }
 
         private void NodeCheckToggled(object sender,EventArgs e)
@@ -51,34 +27,12 @@ namespace FileManager4Broadcasting
             MessageBox.Show(sender.GetType().ToString());
         }
 
-        private List<TreeNode> GetAllChild(TreeNodeCollection Nodes)
-        {
-            List<TreeNode> ar = new List<TreeNode>();
-            foreach (TreeNode node in Nodes)
-            {
-                ar.Add(node);
-                if (node.GetNodeCount(false) > 0)
-                {
-                    ar.AddRange(GetAllChild(node.Nodes));
-                }
-            }
-            return ar;
-        }
-
         private void ImportForm_Shown(object sender, EventArgs e)
         {
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            foreach (TreeNode tn in GetAllChild(rootNode.Nodes))
-            {
-
-                MessageBox.Show(tn.Text);
-            }
-        }
-
+        /*未実装
         private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
         {
 
@@ -95,7 +49,22 @@ namespace FileManager4Broadcasting
                 {
                     e.Node.Parent.Checked = e.Node.Checked;
                 }
+                MessageBox.Show("test");
             }
         }
+
+        private List<TreeNode> GetAllChild(TreeNodeCollection Nodes)
+        {
+            List<TreeNode> ar = new List<TreeNode>();
+            foreach (TreeNode node in Nodes)
+            {
+                ar.Add(node);
+                if (node.GetNodeCount(false) > 0)
+                {
+                    ar.AddRange(GetAllChild(node.Nodes));
+                }
+            }
+            return ar;
+        }*/
     }
 }
